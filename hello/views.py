73c3e45caf6dcbfa241e4fa0264491206c7c005c
Muhammad Hilmi Asyrofi <mhilmiasyrofi@gmail.com>
2021-09-25 06:25:11 +0700

    create id

--- hello/views.py
+++ hello/views.py
@@ -3,10 +3,22 @@ from django.http import HttpResponse
 
 from .models import Greeting
 
+
+import requests
+
+
+# def index(request):
+#     r = requests.get('http://httpbin.org/status/418')
+#     print(r.text)
+#     return HttpResponse('<pre>' + r.text + '</pre>')
+
 # Create your views here.
 def index(request):
+
     # return HttpResponse('Hello from Python!')
-    return render(request, "index.html")
+    if request.method == 'GET':
+        id = request.GET.get('id', 1)
+    return render(request, "index.html", {"id": id})
 
 
 def db(request):
